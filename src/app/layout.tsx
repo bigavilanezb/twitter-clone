603c4ba1528396801ff0a647d6712ff18dc3a111
Bryan Isrrael Gavilanez Bacilio <bigavilanezb00@gmail.com>
2025-10-08 21:34:01 +0200

    Falta por configurar los Trigger de Supabase. La guía que se estaba siguiendo, investigar los Webhooks para seguir avanzado en el desarrollo.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Con de Twitter",
+  title: "Clon de Twitter",
   description: "Generated moviendo las manitas",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
+import { createServerComponentClient} from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers';
+import { AuthButtonServer } from '@/app/components/auth-buton-server';
+import { redirect } from 'next/navigation';
 
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const {data: {session} } = await supabase.auth.getSession()
 
-export default function Home() {
+  if (session == null) {
+    redirect('/login')
+  }
+
+  const {data: posts} = await supabase
+  .from('posts')
+  .select('*, auth.users(email)')
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <AuthButtonServer/>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Hola Twitter!
+        
+        <pre>
+          {
+            JSON.stringify(posts, null, 2)
+          }
+        </pre>
       </main>
     </div>
   );
