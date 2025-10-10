524d200c7f15675076f9ddd7e628e3173a3fd46f
Bryan Isrrael Gavilanez Bacilio <bigavilanezb00@gmail.com>
2025-10-10 13:48:16 +0200

    No sé se ha liado mucho cuanod he intentado instalar el supabase login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,29 @@
-import { createServerComponentClient} from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers';
-import { AuthButtonServer } from '@/app/components/auth-buton-server';
-import { redirect } from 'next/navigation';
-import PostCard from './components/post-card';
-import {  PostLists } from './components/posts-list';
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { AuthButtonServer } from "@/app/components/auth-buton-server";
+import { redirect } from "next/navigation";
+import { PostLists } from "./components/posts-list";
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies })
-  const {data: {session} } = await supabase.auth.getSession()
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
   if (session == null) {
-    redirect('/login')
+    redirect("/login");
   }
 
-  const {data: posts} = await supabase
-  .from('posts')
-  .select('*, user: users(name, avatar_url, user_name)')
+  const { data: posts } = await supabase
+    .from("posts")
+    .select("*, user: users(name, avatar_url, user_name)");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-
-      <section className='max-w-[600px] mx-auto border-l border-r border-white/50 min-h-screen'>
-        <AuthButtonServer/>
-        <PostLists posts={posts}/>
+      <section className="max-w-[600px] mx-auto border-l border-r border-white/20 min-h-screen">
+        <AuthButtonServer />
+        <PostLists posts={posts} />
       </section>
-
     </main>
   );
 }
